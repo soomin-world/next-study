4904e7abc1a5cc7ee1a086a924ee11c2f1c48e73
soomin-world <ojudve123@gmail.com>
2023-06-03 13:06:47 +0900

    메인페이지 레이아웃 구현

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
 /** 메인 페이지 */
 
 export default function Home() {
-  return <div></div>;
+  let name = "soomin";
+  let link = "http://google.com";
+  return (
+    <div>
+      <h4 style={{ color: "red", fontSize: "30px" }}>애플후레시</h4>
+      <p className="title-sub"> by {name}</p>
+      <a href={link}>링크 </a>
+    </div>
+  );
 }
